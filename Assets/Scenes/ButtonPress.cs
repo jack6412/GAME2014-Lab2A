9f1bd4126db3dafc1bc78f383a61b5eb22ac5927
jack6412 <jack6chang@gmail.com>
2021-09-21 15:56:35 -0400

    create project
    create lab2

---
+++ Assets/Scenes/ButtonPress.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Debug.Log("Start button press!");
+    }
+}
